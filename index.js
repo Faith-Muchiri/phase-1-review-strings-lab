79c54d93d06b2bc28bc1ac2d04193d7d7579d7e5
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-08-15 18:16:29 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+const currentUser = 'Fayze Muchiri';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// const shortGreeting = `Welcome ${currentUser}.charAt(0)!`
+
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
+
+
+
+
